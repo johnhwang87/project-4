c3720999af8a61e7af11966b26bfc9b4cd6b962c
John Hwang <johnsies87@gmail.com>
2016-02-15 23:44:49 -0800

    adding camera-ui plugin and some snapping... awaiting test..

--- client/scripts/controllers/profile.controller.js
+++ client/scripts/controllers/profile.controller.js
@@ -2,7 +2,7 @@ angular
   .module('Project4')
   .controller('ProfileCtrl', ProfileCtrl);
 
-function ProfileCtrl ($scope, $reactive, $state, $ionicPopup, $log) {
+function ProfileCtrl ($scope, $reactive, $state, $ionicPopup, $log, $ionicLoading) {
   $reactive(this).attach($scope);
   // defining Meteor user
   let user = Meteor.user();
@@ -10,7 +10,30 @@ function ProfileCtrl ($scope, $reactive, $state, $ionicPopup, $log) {
 
   this.name = name;
   this.updateName = updateName;
+  this.updatePicture = updatePicture;
 
+  // implementing controller methods that use camera-ui API to get image from device, and then run server method for updating the image
+    function updatePicture () {
+    MeteorCameraUI.getPicture({ width: 60, height: 60 }, function (err, data) {
+     // what is this sorcery...
+      if (err && err.error == 'cancel') {
+        return;
+      }
+
+      if (err) {
+        return handleError(err);
+      }
+
+      $ionicLoading.show({
+        template: 'Updating picture...'
+      });
+      // calling data from the server method, receiving data back from server
+      Meteor.call('updatePicture', data, (err) => {
+        $ionicLoading.hide();
+        handleError(err);
+      });
+    });
+  }
 
   // function for updating user name, which is a server method. So, this must be added unto methods.js
   function updateName () {
