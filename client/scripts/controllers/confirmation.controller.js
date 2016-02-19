cd1ba842af396ebbffa4a86836dc9e5e18f544d2
John Hwang <johnsies87@gmail.com>
2016-02-19 01:31:50 -0800

    styling

--- client/scripts/controllers/confirmation.controller.js
+++ client/scripts/controllers/confirmation.controller.js
@@ -2,17 +2,17 @@ angular
   .module('Project4')
   .controller('ConfirmationCtrl', ConfirmationCtrl);
 
+
 function ConfirmationCtrl($scope, $reactive, $state, $ionicPopup, $log) {
   $reactive(this).attach($scope);
 
   this.phone = $state.params.phone;
   this.confirm = confirm;
 
-
   // adding confirmation for the pin. if accepted, leads to profile page
   function confirm() {
+    // Accounts.phoneVerificationMasterCode('1234');
     if (_.isEmpty(this.code)) return;
-
     Accounts.verifyPhone(this.phone, this.code, function (err) {
       if (err) return handleError(err);
       $state.go('profile');
--- server/sms.js
+++ server/sms.js
@@ -4,10 +4,18 @@
 //   AUTH_TOKEN: Meteor.settings.TWILIO.a5655c01b3738917173ff9be260f93fc
 // };
 
+// SMS.twilio = {
+
+//      ACCOUNT_SID: Meteor.settings.TWILIO.SID,
+//      AUTH_TOKEN: Meteor.settings.TWILIO.TOKEN
+// };
+
 if (Meteor.isServer) {
 // configuring to serverside to make this work
     if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
       Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
       Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
+
+      // Accounts._options.phoneVerificationMasterCode = '1234'
     }
 }
